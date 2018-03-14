fbc8391c7f15d13b9d262f09316b817cfb15d63b
hirotakasato <hukutsumania@gmail.com>
2018-03-14 10:29:07 +0000

    week3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,34 @@
 class BlogsController < ApplicationController
 
 	def index
+		@blogs = Blog.all
 	end
 	
+	def new
+		@blog = Blog.new
+		# @つけると、対応するviewで使える。
+		# ブログも出ると紐づいた（データを入れるための）からの箱を定義する。
+		# インスタンスという。
+	end
+
+	def create
+		blog = Blog.new(blog_params)
+		# ビューから送られてきたデータをblogという空き箱に入れている。
+		blog.save
+		redirect_to '/blogs'
+		# 他のビューに移動するメソッド
+	end
+
+	def show
+		@blog = Blog.find(params[:id])
+		# findメソッドはidを使ってモデルからレコードを探し出すメソッド
+	end
+
+	private
+		def blog_params
+			params.require(:blog).permit(:title, :body, :category)
+		end
+		# ビューから送信されたデータはparamsというメソッドの中に入っている。
+		# ストロングパラメータは、セキュリティを強固にするもの。
+
 end
