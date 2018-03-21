a1cd31c578dd91d3bda9bf1e12efeb5821f9cda6
hirotakasato <hukutsumania@gmail.com>
2018-03-21 10:46:36 +0000

    heroku deploy

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem "pg", group: :production
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
@@ -18,7 +21,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'pry-rails'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,6 +17,8 @@ class BlogsController < ApplicationController
 		blog.save
 		redirect_to '/blogs'
 		# 他のビューに移動するメソッド
+		# binding.pry
+		# コンソールに入りたい場所でこれを記述
 	end
 
 	def show
@@ -24,9 +26,30 @@ class BlogsController < ApplicationController
 		# findメソッドはidを使ってモデルからレコードを探し出すメソッド
 	end
 
+	def edit
+		@blog = Blog.find(params[:id])
+	end
+
+	def update
+		blog = Blog.find(params[:id])
+		blog.update(blog_params)
+		# redirect_to '/blogs/#{blog.id}'
+		redirect_to blog_path(blog.id)
+		# rake routesで確認できるprefix(名前付きパス)を使用
+		# 別解
+		# redirect_to blog_path(blog)
+		# redirect_to blog
+	end
+
+	def destroy
+		blog = Blog.find(params[:id])
+		blog.destroy
+		redirect_to blogs_path
+	end
+
 	private
 		def blog_params
-			params.require(:blog).permit(:title, :body, :category)
+			params.require(:blog).permit(:title, :body, :category, :blog_image)
 		end
 		# ビューから送信されたデータはparamsというメソッドの中に入っている。
 		# ストロングパラメータは、セキュリティを強固にするもの。
